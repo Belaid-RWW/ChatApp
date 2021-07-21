6fd1eea440e208f749406d640ddcdd25ac8b84a9
Fedy Belaid <fedybelaid1902@gmail.com>
2021-07-21 07:23:29 +0100

    Added layouts

--- app/src/main/java/com/example/chatapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.chatapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.chatapp.R
+import io.getstream.chat.android.client.ChatClient
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private val client = ChatClient.instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        if(navController.currentDestination?.label.toString().contains("login")){
+            
+        }
     }
 }
